72d0fbd3239eb219d7250a5292ee7aad99c01127
Z3r0c0k3 <hackerj1128@gmail.com>
2024-11-06 12:26:09 +0900

    fix: modified ubuntu_2*/peda_auto_set.sh

--- ubuntu_22.04/peda_auto_set.sh
+++ ubuntu_22.04/peda_auto_set.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "source /home/$USER/peda/peda.py" > /home/$USER/.gdbinit; \
-chown $USER:$USER /home/$USER/.gdbinit; \
-cp -r /root/peda /home/$USER/peda && chown -R $USER:$USER /home/$USER/peda; \
-cp -r /app/PWNABLE /home/$USER/PWNABLE && chown -R $USER:$USER /home/$USER/PWNABLE; \
-cp -r /app/PWNABLE2 /home/$USER/PWNABLE2 && chown -R $USER:$USER /home/$USER/PWNABLE2; \
+sudo cp -r /root/peda /home/$USER/peda && sudo chown -R $USER:$USER /home/$USER/peda; \
+sudo cp -r /root/PWNABLE /home/$USER/PWNABLE && sudo chown -R $USER:$USER /home/$USER/PWNABLE; \
+sudo cp -r /root/PWNABLE2 /home/$USER/PWNABLE2 && sudo chown -R $USER:$USER /home/$USER/PWNABLE2; \
