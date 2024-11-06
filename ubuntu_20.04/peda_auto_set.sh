aa7f788f8fd20cd3682aac6fb6ad7681024f79f5
Z3r0c0k3 <hackerj1128@gmail.com>
2024-11-06 12:20:53 +0900

    fix: modified ubuntu_2*/peda_auto_set.sh

--- ubuntu_20.04/peda_auto_set.sh
+++ ubuntu_20.04/peda_auto_set.sh
@@ -3,3 +3,5 @@
 echo "source /home/$USER/peda/peda.py" > /home/$USER/.gdbinit; \
 chown $USER:$USER /home/$USER/.gdbinit; \
 cp -r /root/peda /home/$USER/peda && chown -R $USER:$USER /home/$USER/peda; \
+cp -r /app/PWNABLE /home/$USER/PWNABLE && chown -R $USER:$USER /home/$USER/PWNABLE; \
+cp -r /app/PWNABLE2 /home/$USER/PWNABLE2 && chown -R $USER:$USER /home/$USER/PWNABLE2; \
