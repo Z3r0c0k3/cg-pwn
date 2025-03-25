34eb3a3057164c991f4a90e96b31e980e043f094
Z3r0c0k3 <hackerj1128@gmail.com>
2025-03-25 16:38:13 +0900

    fix: added gdb-peda

--- ubuntu_18.04/peda_auto_set.sh
+++ ubuntu_18.04/peda_auto_set.sh
@@ -2,5 +2,5 @@
 
 echo "source /home/$USER/peda/peda.py" > /home/$USER/.gdbinit; \
 sudo cp -r /root/peda /home/$USER/peda && sudo chown -R $USER:$USER /home/$USER/peda; \
-sudo cp -r /root/PWNABLE /home/$USER/PWNABLE && sudo chown -R $USER:$USER /home/$USER/PWNABLE; \
-sudo cp -r /root/PWNABLE2 /home/$USER/PWNABLE2 && sudo chown -R $USER:$USER /home/$USER/PWNABLE2; \
+sudo cp -r /app/PWNABLE /home/$USER/PWNABLE && sudo chown -R $USER:$USER /home/$USER/PWNABLE; \
+sudo cp -r /app/PWNABLE2 /home/$USER/PWNABLE2 && sudo chown -R $USER:$USER /home/$USER/PWNABLE2; \
